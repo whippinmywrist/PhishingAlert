bbd43a39a90b0c780c95528512705e177e1e8ab9
antonskvorcov <azicazic@yandex.ru>
2021-01-20 22:48:46 +0300

    Frontend update

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/modules')
+def modules():
+    return render_template('modules.html')
+
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run()
